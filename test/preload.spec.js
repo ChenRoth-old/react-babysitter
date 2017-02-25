c0674f6b64bf18f7d827ae8d52543b09b60e7dfd
Chen Roth <rothchen@gmail.com>
2017-02-25 16:25:16 +0200

    work starts here

--- test/preload.spec.js
+++ test/preload.spec.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { shallow, render } from 'enzyme'
 import { expect } from 'chai'
 import Component from './Component'
+import preload from '../src'
 
 describe('preload', () => {
   it('should pass-through component without args', () => {
-    const component = render(<Component />)
-    const passThrough = render(<Component />)
-    expect(component.html()).to.equal(passThrough.html())
+    const component = <Component />
+    const passThrough = preload()(<Component />)
+    expect(render(passThrough).html()).to.equal(render(component).html())
   })
 })
